c3dfb76c90c504098378d3ac8d44c3e6dee15882
alirezakayvan <54979015+alirezakayvan@users.noreply.github.com>
2025-03-06 12:12:59 +0330

    Add comments to File

--- monitor.sh
+++ monitor.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "CPU Usage:"
-top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}' | xargs echo "%"
+top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}' | xargs echo "%"  # Displays CPU usage percentage
 
 echo "Memory Usage:"
-free -h | awk '/Mem:/ {print $3 " / " $2}'
+free -h | awk '/Mem:/ {print $3 " / " $2}'  # Displays used and total memory
 
 echo "Disk Usage:"
-df -h | grep '^/dev/' | awk '{print $1 " : " $5}'
+df -h | grep '^/dev/' | awk '{print $1 " : " $5}'  # Shows disk usage for mounted drives
